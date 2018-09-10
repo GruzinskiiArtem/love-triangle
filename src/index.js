2344877a1d55d1d5ae8174719683e5b4d31dc9ae
GruzinskiiArtem <m1nck@yandex.ru>
2018-09-11 01:07:35 +0300

    Correction of error.

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
     for (var i = 0; i < preferences.length; i++) {
         let firstElement = preferences[i];
         let secondElement = preferences[firstElement-1];
-        if (firstElement == i + 1 || secondElement == firstElement || preferences[secondElement-1] == secondElement-1) {
+        if (firstElement == i + 1 || secondElement == firstElement || preferences[secondElement - 1] == secondElement - 1) {
             continue;
         }
-        if( i++ == preferences[secondElement-1]) {
+        if( i + 1 == preferences[secondElement - 1]) {
             triangles++;
             preferences[i] = false;
             preferences[firstElement - 1] = false;
