2cd5a063a29edc72d5d7aa5ec8bc0f1279dbb6ff
GruzinskiiArtem <m1nck@yandex.ru>
2018-09-11 00:50:41 +0300

    Calculating triangles in an array.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let triangles = 0;
+    for (var i = 0; i < preferences.length; i++) {
+        let firstElement = preferences[i];
+        let secondElement = preferences[firstElement-1];
+        if (firstElement == i + 1 || secondElement == firstElement || preferences[secondElement-1] == secondElement-1) {
+            continue;
+        }
+        if( i++ == preferences[secondElement-1]) {
+            triangles++;
+            preferences[i] = false;
+            preferences[firstElement - 1] = false;
+            preferences[secondElement - 1] = false;
+        }
+    }
+    return triangles;
 };
